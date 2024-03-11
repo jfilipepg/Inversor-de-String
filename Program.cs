7747452128486e5fe3c360a25bf80595b4ca23f2
jfilipepg <103340995+jfilipepg@users.noreply.github.com>
2024-03-11 10:05:35 -0300

    testando meu programa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 5) Escreva um programa que inverta os caracteres de um string.
+string invertida, auxiliar = "";
+int i;
+
+Console.Write ("Informe a string que você deseja que seja invertida: ");
+invertida = Console.ReadLine();
+
+for(i = invertida.Length - 1; i >= 0; i--){
+    auxiliar += invertida[i];
+}
+
+invertida = auxiliar;
+Console.Write(invertida);
